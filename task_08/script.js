9cf89b26a3186e040103aa58135a518ca9adb952
Anastasia Makus <asya.makus@gmail.com>
2025-07-14 21:34:31 +0300

    task_08

--- task_08/script.js
+++ task_08/script.js
@@ -1,22 +1,29 @@
 let res = sumDigits(1234);
-
 console.log(`sum digits = ${res}`);
 
 res = luckyNumber(123871); // 1 + 3 + 7 === 2 + 8 + 1
-
 console.log(res ? 'Lucky' : "Unlucky"); // Lucky
 
 res = luckyNumber(153871); // 1 + 3 + 7 !== 5 + 8 + 1
-
 console.log(res ? 'Lucky' : `Unlucky`); // Unlucky
 
 function sumDigits(a) {
-    return a.toString()
-        .split('')
-        .map(Number)
-        .reduce((a, b) => a + b, 0);
+    return a.toString().split('').map(Number).reduce((a, b) => a + b, 0);
 }
 
 function luckyNumber(a) {
-//TODO
+    let firstNumber = 0;
+    let secondNumber = 0;
+    let result = a.toString().split('').map(Number);
+
+    result.forEach((item, i) => {
+        if (i % 2 === 0) {
+            firstNumber += item;
+        } else {
+            secondNumber += item;
+        }
+    });
+
+    return firstNumber === secondNumber
+
 }
