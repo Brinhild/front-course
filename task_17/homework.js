d6da63b9acec3dbc8f09c2f31a164c7eaa3d6bfc
Anastasia Makus <asya.makus@gmail.com>
2025-07-30 13:17:31 +0300

    task-17

---
+++ task_17/homework.js
@@ -0,0 +1,14 @@
+// Problem -> console 10 times 10
+// for (var i = 0; i < 10; i++) {
+//     setTimeout(function () {
+//         console.log(i);
+//     }, 1000)
+// }
+
+for (var i = 0; i < 10; i++) {
+    (function(i) {
+        setTimeout(function () {
+            console.log(i);
+        }, i * 1000);
+    })(i);
+}
